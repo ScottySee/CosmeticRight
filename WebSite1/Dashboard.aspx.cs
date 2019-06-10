0fdc26f5199740459442a16bdd9ceb92371ba5cd
Scotty Wilburt See <scottywilburt.see@benilde.edu.ph>
2019-06-10 15:30:43 +0800

    June 10

--- WebSite1/Dashboard.aspx.cs
+++ WebSite1/Dashboard.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,6 +10,104 @@ public partial class Dashboard : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+        {
+            GetWebsiteCount();
+            GetOrderCount();
+            GetUserCount();
+            GetSales();
+        }
+    }
 
+    void GetWebsiteCount()
+    {
+        using (SqlConnection con = new SqlConnection(Util.GetConnection()))
+        {
+            con.Open();
+            string query = @"select count(*) as Count from WebsiteVisit";
+
+            using (SqlCommand cmd = new SqlCommand(query, con))
+            {
+                using (SqlDataReader data = cmd.ExecuteReader())
+                {
+                    if (data.HasRows)
+                    {
+                        while (data.Read())
+                        {
+                            count.Text = data["Count"].ToString();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    void GetOrderCount()
+    {
+        using (SqlConnection con = new SqlConnection(Util.GetConnection()))
+        {
+            con.Open();
+            string query = @"select count(*) as Count from Orders";
+
+            using (SqlCommand cmd = new SqlCommand(query, con))
+            {
+                using (SqlDataReader data = cmd.ExecuteReader())
+                {
+                    if (data.HasRows)
+                    {
+                        while (data.Read())
+                        {
+                            count1.Text = data["Count"].ToString();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    void GetUserCount()
+    {
+        using (SqlConnection con = new SqlConnection(Util.GetConnection()))
+        {
+            con.Open();
+            string query = @"select count(*) as Count from Users";
+
+            using (SqlCommand cmd = new SqlCommand(query, con))
+            {
+                using (SqlDataReader data = cmd.ExecuteReader())
+                {
+                    if (data.HasRows)
+                    {
+                        while (data.Read())
+                        {
+                            count2.Text = data["Count"].ToString();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    void GetSales()
+    {
+        using (SqlConnection con = new SqlConnection(Util.GetConnection()))
+        {
+            con.Open();
+            string query = @"SELECT SUM(Amount) AS Total FROM OrderDetails";
+
+            using (SqlCommand cmd = new SqlCommand(query, con))
+            {
+                using (SqlDataReader data = cmd.ExecuteReader())
+                {
+                    if (data.HasRows)
+                    {
+                        while (data.Read())
+                        {
+                            sales.Text = data["Total"].ToString();
+                        }
+                    }
+                }
+            }
+        }
     }
 }
